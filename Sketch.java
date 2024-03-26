ad001d4e40dcfb6a79d17fc831c45375131f9a9f
penre <tianyou.zhang25@ycdsbk12.ca>
2024-03-26 14:29:21 -0400

    finished till 6

--- Sketch.java
+++ Sketch.java
@@ -64,8 +64,8 @@ public class Sketch extends PApplet {
 
     for (int intRow = 0; intRow < 30; intRow++) {
       for (int intColumn = 0; intColumn < 30; intColumn++) {
-        intX = 3 + 0;  // Instead of zero, calculate the proper intX location using 'intRow'
-        intY = 300 + 3 + 0; // Instead of zero, calculate the proper intY location using 'intColumn'
+        intX = 3 + intRow * 10;  // Instead of zero, calculate the proper intX location using 'intRow'
+        intY = 300 + 3 + intColumn * 10;  // Instead of zero, calculate the proper intY location using 'intColumn'
 
         fill(255);
         noStroke();
@@ -79,30 +79,101 @@ public class Sketch extends PApplet {
    * Don't loop from 30 to 60 to shift everything over, just add 300 to x.
    */
   public void draw_section2() {
+    
+    int intX = 0;
+    int intY = 0;
 
-  }
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+        intX = 3 + 300 + intRow * 10;
+        intY = 300 + 3 + intColumn * 10;
+
+        if (intRow % 2 == 0){
+          fill(255);
+        }
+        else { 
+          fill(0);
+        }
+        noStroke();
+        rect(intX,intY, 5, 5);
+      }
+    }
+  } 
+    
 
   /**
    * Use the modulus operator and an if/else statement to select the color.
    * Don't use multiple 'if' statements.
    */
   public void draw_section3() {
+    int intX = 0;
+    int intY = 0;
 
-  }
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+        intX = 3 + 600 + intRow * 10;
+        intY = 300 + 3 + intColumn * 10;
+
+        if (intColumn % 2 == 0){
+          fill(0);
+        }
+        else { 
+          fill(255);
+        }
+        noStroke();
+        rect(intX,intY, 5, 5);
+      }
+    }
+  } 
+
+  
 
   /**
    * Use the modulus operator and just one 'if' statement to select the color.
    */
   public void draw_section4() {
+    int intX = 0;
+    int intY = 0;
 
-  }
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+        intX = 3 + 900 + intRow * 10;
+        intY = 300 + 3 + intColumn * 10;
+        fill (255);
+      
+        if (intRow % 2 == 1 || intColumn % 2 == 0){
+          fill(0);
+        }
+
+        noStroke();
+        rect(intX,intY, 5, 5);
+      }
+    }
+  } 
+
+  
 
   /**
    * Do NOT use 'if' statements to complete 5-8. Manipulate the loops instead
    */
   public void draw_section5() {
+  
+    int intX = 0;
+    int intY = 0;
 
+    for (int intRow = 0; intRow < 30; intRow++) {
+      for (int intColumn = 0; intColumn < 30; intColumn++) {
+        intX = 3 + intRow * 10;  
+        intY = 3 + intColumn * 10;  
+
+        fill(255);
+        noStroke();
+        rect(intX, intY, 5, 5);
+      }
+    }
   }
+
+  
 
   public void draw_section6() {
 
